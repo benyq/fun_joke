deab67ff0e9f85a8f24dc237cd623a8cc3b587b2
benyq <yzjbenyq@gmail.com>
2024-06-28 17:53:44 +0800

    search文件创建

---
+++ lib/business/home/search/serach_view_model.dart
@@ -0,0 +1,12 @@
+import 'package:fun_joke/models/joke_detail_model.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'serach_view_model.g.dart';
+
+@riverpod
+class SearchVM extends _$SearchVM {
+  List<JokeDetailModel> build() {
+    return const [];
+  }
+
+}
